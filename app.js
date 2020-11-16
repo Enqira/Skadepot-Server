4eceb07b6bbe90ea6ea5ea7e660d22e5dbb7a9fe
Enqira <moenkira@yahoo.com>
2020-11-16 11:42:33 +0100

    changed listening port parameters

--- app.js
+++ app.js
@@ -144,9 +144,9 @@ const account = {
 
 
 
-app.listen(3000, () => {
-  console.log('Server listening on http://localhost:3000 ...');
-});
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
 
 
 
