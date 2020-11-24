1dbb5ac1b2b33b875a9c5dda74d7146b2e31d89c
Enqira <moenkira@yahoo.com>
2020-11-24 02:20:48 +0100

    changes in login router, using user name instead of email to log in

--- routes/login.js
+++ routes/login.js
@@ -7,15 +7,18 @@ const jwt = require("jsonwebtoken");
 // post for login
 router.post("/login", async (req, res) => {
   // Check if email exist en db
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("'Email' or password is wrong");
+  const user = await User.findOne({ name: req.body.name });
+  console.log(req.body.name);
+
+  if (!user) return res.status(400).send("'name' or password is wrong");
   // check if password is correct
   const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return res.status(400).send("Email or 'password' is wrong");
+  if (!validPass) return res.status(400).send("name or 'password' is wrong");
 
   //Create and assign a token
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
   res.header("auth-token", token).send(token);
+  //   res.status(200).send("loged in");
 
   console.log("logged in");
 });
