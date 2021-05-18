41d707a0297d6606ac18353b75cf3e748a3cba25
Enqira <53911763+Enqira@users.noreply.github.com>
2021-05-18 14:23:39 +0200

    cleaner code from comments and logs

--- routes/login.js
+++ routes/login.js
@@ -1,26 +1,22 @@
-const router = require("express").Router();
-const mongoose = require("mongoose");
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const router = require("express").Router()
+const mongoose = require("mongoose")
+const User = require("../models/User")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 // post for login
 router.post("/login", async (req, res) => {
   // Check if user exist en db
-  const user = await User.findOne({ name: req.body.name });
-  console.log(req.body.name);
+  const user = await User.findOne({ name: req.body.name })
 
-  if (!user) return res.status(400).send("'name' or password is wrong");
+  if (!user) return res.status(400).send("'name' or password is wrong")
   // check if password is correct
-  const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return res.status(400).send("name or 'password' is wrong");
+  const validPass = await bcrypt.compare(req.body.password, user.password)
+  if (!validPass) return res.status(400).send("name or 'password' is wrong")
 
   //Create and assign a token
-  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-  res.header("auth-token", token).send(token);
-  //   res.status(200).send("loged in");
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
+  res.header("auth-token", token).send(token)
+})
 
-  console.log("logged in");
-});
-
-module.exports = router;
+module.exports = router
